4ee9d4f1dcb50aeee9cf22ffcdb2cd15c697de99
Abhijit47 <akarmakar846@gmail.com>
2024-09-16 14:52:32 +0530

    Refactor loading components and update SVG class names

--- app/loding.jsx
+++ app/loding.jsx
@@ -4,7 +4,7 @@ export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
   return (
     <div className="grid h-screen place-items-center">
-      <SvgSpinners3DotsScale className="h-20 w-20" />;
+      <SvgSpinners3DotsScale className="w-20 h-20" />
     </div>
   );
 }
